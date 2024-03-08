7cd97773fe06740105e178391c6021353416f72c
devavaz <135300758+devavaz@users.noreply.github.com>
2024-03-08 14:09:39 +0900

    added and styled Sidebar with materialUI icons

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,14 @@
+import Siderbar from "../../components/sidebar/Siderbar"
 import "./home.scss"
 
 const Home = () => {
   return (
-    <div className="home">Home</div>
+    <div className="home">
+        <Siderbar />
+        <div className="homeContainer">
+            container
+        </div>
+    </div>
   )
 }
 
