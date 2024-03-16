ac7ad22ce29ab91622e47aeecff04a97af1e5c90
devavaz <135300758+devavaz@users.noreply.github.com>
2024-03-16 10:00:56 +0900

    Added table using material ui table comp

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,30 +1,35 @@
-import Chart from "../../components/chart/Chart"
-import Featured from "../../components/featured/Featured"
-import Navbar from "../../components/navbar/Navbar"
-import Siderbar from "../../components/sidebar/Siderbar"
-import Widget from "../../components/widget/Widget"
+import Chart from "../../components/chart/Chart";
+import Featured from "../../components/featured/Featured";
+import Navbar from "../../components/navbar/Navbar";
+import Siderbar from "../../components/sidebar/Siderbar";
+import Widget from "../../components/widget/Widget";
+import Table from "../../components/table/Table";
 
-import "./home.scss"
+import "./home.scss";
 
 const Home = () => {
   return (
     <div className="home">
-        <Siderbar />
-        <div className="homeContainer">
-            <Navbar />
-            <div className="widgets">
-              <Widget type="user" />
-              <Widget type="order" />
-              <Widget type="earnings" />
-              <Widget type="balance" />  
-            </div>
-            <div className="charts">
+      <Siderbar />
+      <div className="homeContainer">
+        <Navbar />
+        <div className="widgets">
+          <Widget type="user" />
+          <Widget type="order" />
+          <Widget type="earnings" />
+          <Widget type="balance" />
+        </div>
+        <div className="charts">
           <Featured />
           <Chart title="Last 6 Months (Revenue)" />
-            </div>
         </div>
+        <div className="listContainer">
+          <div className="listTitle">Latest Transactions</div>
+          <Table />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
