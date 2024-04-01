bf4c0f91657f2d5659cdef2c926c778d6d58c74e
devavaz <135300758+devavaz@users.noreply.github.com>
2024-04-01 22:56:16 +0900

    Added users/new page to add new users and added links inside sidebar

--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -4,6 +4,7 @@ import Navbar from "../../components/navbar/Navbar";
 import Chart from "../../components/chart/Chart";
 import List from "../../components/table/Table";
 
+
 const Single = () => {
   return (
     <div className="single">
